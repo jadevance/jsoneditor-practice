5c0e0a11f849ab7e40436e8e1de60e165784fd59
Jade Vance <jadenicolevance@gmail.com>
2016-12-01 14:34:11 -0800

    refactoring, moving to modular functions

--- jsoneditor.js
+++ jsoneditor.js
@@ -1,73 +1,96 @@
 function Editor() {
-  var self = this; 
-  //  create the code editor
-  var codeContainer = document.getElementById("codeEditor");
-  var codeOptions = {
-      mode: 'code',
-      onError: function (err) {
-      alert(err.toString());
-     }
-  };
+  var self = this;
 
-  // create the tree viewer
-  var viewContainer = document.getElementById("treeViewer");
-  var viewOptions = {
-      mode: 'tree',
-      onError: function (err) {
-          alert(err.toString());
-      },
-      onChange: function (change) { 
-        // write this tomorrow, update the json with changes 
-      },
-      search: true
-  };
+  this.codeContainer  = null;
+  this.treeContainer  = null;
+  this.jsonEditor     = null;
+  this.jsonTree       = null;
 
-  var editor = new JSONEditor(codeContainer, codeOptions);
-  var viewer = new JSONEditor(viewContainer, viewOptions);
 
-  // get json, will retrieve from controller eventually 
-  var json = {
-      "Array": [1, 2, 3],
-      "Boolean": true,
-      "Null": null,
-      "Number": 123,
-      "Object": {"a": "b", "c": "d"},
-      "String": "Hello World"
-  };
-
-  // set json
-  editor.set(json);
-  viewer.set(json);
-
-  // get json from view 
-  var jsonEditor = editor.get();
-  var jsonViewer = viewer.get();
-
-  // Load a JSON document
-  FileReaderJS.setupInput(document.getElementById('loadDocument'), {
-      readAsDefault: 'Text',
-      on: {
-          load: function (event, file) {
-              editor.setText(event.target.result);
-          }
+  this.jsonImport = FileReaderJS.setupInput(document.getElementById('loadDocument'), {
+    readAsDefault: 'Text',
+    on: {
+      load: function (event, file) {
+        self.jsonEditor.setText(event.target.result);
       }
+    }
   });
 
-  // Save a JSON document
-  document.getElementById('saveDocument').onclick = function () {
+  // default 
+  this.json = {
+    "Array": [1, 2, 3],
+    "Boolean": true,
+    "Null": null,
+    "Number": 123,
+    "Object": { "a": "b", "c": "d" },
+    "String": "Hello World",
+  };
 
-    // Save Dialog
-    fname = window.prompt("Save as...");
+  this.init = function(selector) {
+    self.codeContainer = $(selector)
+    var codeOptions = {
+      mode: 'code',
+      onError: function(error) {
+          alert(error.toString());
+    },
+      search: true
+    }
 
-    // Check json extension in file name
-    if (fname.indexOf(".") == -1) {
-        fname = fname + ".json";
-    } else {
-        if (fname.split('.').pop().toLowerCase() == "json") {
-            // Nothing to do
-        } else {
-            fname = fname.split('.')[0] + ".json";
-        }
-    }; 
+    self.jsonEditor = new JSONEditor(codeContainer, codeOptions, self.json);
+    self.buildTreeViewer();
+    $("#toTree").click(function() { self.updateTree() });
+    $("#toCode").click(function() { self.updateCode() });
   }
-}
+
+  this.updateTree = function() {
+    var json = self.jsonEditor.get();
+    self.jsonEditor.set(json);
+    self.jsonTree.set(json);
+  }
+
+  this.updateCode = function() {
+    var json = self.jsonTree.get();
+    self.jsonTree.set(json);
+    self.jsonEditor.set(json);
+  }
+
+
+  this.setJson = function() {
+    self.jsonEditor = self.jsonEditor.get()
+    self.buildTreeViewer();
+  }
+
+  this.buildTreeViewer = function() {
+    self.treeContainer = document.getElementById("treeContainer");
+
+    var treeOptions = {
+      mode: 'tree',
+      onError: function (err) {
+        alert(err.toString());
+      },
+      search: true
+    };
+    self.jsonTree = new JSONEditor(self.treeContainer, treeOptions, self.json);
+  }
+};
+
+
+    // // Save a JSON document
+    // document.getElementById('saveDocument').onclick = function () {
+
+    //     // Save Dialog
+    //     fname = window.prompt("Save as...");
+
+    //     // Check json extension in file name
+    //     if (fname.indexOf(".") == -1) {
+    //         fname = fname + ".json";
+    //     } else {
+    //         if (fname.split('.').pop().toLowerCase() == "json") {
+    //             // Nothing to do
+    //         } else {
+    //             fname = fname.split('.')[0] + ".json";
+    //         }
+    //     }
+    //     var blob = new Blob([jsonEditor.getText()], { type: 'application/json;charset=utf-8' });
+    //     saveAs(blob, fname);
+    // };
